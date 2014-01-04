49791080999a32e84c73de7a98eef3628b0e5dec
th <trabillard@laposte.net>
2014-01-04 20:38:38 +0100

    Fix bastien bugs + tests

--- TestMoteursDue/TestMoteursDue.ino
+++ TestMoteursDue/TestMoteursDue.ino
@@ -7,50 +7,60 @@ arduino-1.5.5\hardware\arduino\sam\variants\arduino_due_x\variant.h L182
 !!!!
 */
 
-const int sensG = 2;
+const int sensG = 8; //2
 const int pwmG = 31;
-const int sensD = 3;
+const int bG = 32;
+const int sensD = 9; //3
 const int pwmD = 33;
+const int bD = 34;
 
 const int minPwm = 0;
-const int neutralPwm = 2047
+const int neutralPwm = 2047;
 const int maxPwm = 4095;
-const int step = 5;
+const int step = 1;
+const int tempo = 1;
 
 void setup() 
 {   
    pinMode(pwmG, OUTPUT);  
    pinMode(pwmD, OUTPUT);  
+   pinMode(bG, OUTPUT);  
+   pinMode(bD, OUTPUT);  
    
    digitalWrite(pwmG, HIGH); //HIGH = on, LOW = off
    digitalWrite(pwmD, HIGH); //HIGH = on, LOW = off
+   digitalWrite(bG, LOW);
+   digitalWrite(bD, LOW); 
    
    analogWriteResolution(12);
-   analogWrite(sensG, neutralPwm);
-   analogWrite(sensD, neutralPwm);
+   //analogWrite(sensG, neutralPwm);
+   //analogWrite(sensD, neutralPwm);
      
    Serial.begin(115200);
    
    Serial.print("Start your engines");
    
-  for(int i = 5; i > 0; --i)
+  /*for(int i = 5; i > 0; --i)
   {
     Serial.println(i);
     delay(1000);
-  }
+  }*/
    
    Serial.print("Go go go");
-   
-
+    
+  analogWrite(sensG, neutralPwm + (maxPwm - neutralPwm) * .2);
+  analogWrite(sensD, neutralPwm + (maxPwm - neutralPwm) * .2);
 }
 
 void loop() 
 {
+  
+    /*
     for(int i = neutralPwm; i < maxPwm; i+=step)
     {
       analogWrite(sensG, i);
       analogWrite(sensD, i);
-      delay(5);
+      delay(tempo);
       Serial.println(i);
     }
     
@@ -60,7 +70,7 @@ void loop()
     {
       analogWrite(sensG, i);
       analogWrite(sensD, i);
-      delay(5);
+      delay(tempo);
       Serial.println(i);
     }
     
@@ -74,7 +84,7 @@ void loop()
     {
       analogWrite(sensG, i);
       analogWrite(sensD, i);
-      delay(5);
+      delay(tempo);
       Serial.println(i);
     }
     
@@ -82,14 +92,15 @@ void loop()
     
     for(int i = minPwm; i >= neutralPwm; i+=step)
     {
-      analogWrite(pwmG, i);
-      analogWrite(pwmD, i);
-      delay(5);
+      analogWrite(sensG, i);
+      analogWrite(sensD, i);
+      delay(tempo);
       Serial.println(i);
     }
     
-    analogWrite(pwmG, neutralPwm);
-    analogWrite(pwmD, neutralPwm);
+    analogWrite(sensG, neutralPwm);
+    analogWrite(sensD, neutralPwm);
     
     delay(5000);
+    */
 }
